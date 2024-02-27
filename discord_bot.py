e55e0136dffcee9b46ebd359c6b91909622d9ef4
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-28 02:11:07 +0530

    ok

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,40 @@ import discord
 import requests
 import os
 import io
-from .x import GeminiChat
+
+def GeminiChat(prompt):
+    # Replace "YOUR_API_KEY" with your actual API key
+    API_KEY = os.environ.get("Gemini_Key")
+
+    # Define the request data
+    data = {
+        "contents": [
+            {
+                "role": "user",
+                "parts": [{"text": prompt}],
+            }
+        ]
+    }
+
+    # Set the request headers
+    headers = {"Content-Type": "application/json"}
+
+    # Construct the API endpoint URL with the API key
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key={API_KEY}"
+
+    # Send the POST request
+    response = requests.post(url, headers=headers, json=data)
+
+    # Check for successful response
+    if response.status_code == 200:
+    # Parse the JSON response
+        response_data = response.json()
+        generated_text = response_data["candidates"][0]['content']['parts'][0]['text']
+    # Access the generated text from the response
+        return generated_text
+    
+    else:
+        return {"Error" : response.status_code}
 
 IMAGE_API_URL = "https://api-inference.huggingface.co/models/segmind/Segmind-Vega"
 headers = {"Authorization": "Bearer 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"}
