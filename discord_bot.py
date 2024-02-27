b5373b92eda0134576ab0e031e5d4763431fc2b5
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-27 20:41:41 +0530

    ok

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,59 @@ import discord
 import requests
 import os
 import io
-from x import youchat
+import click
+from requests import get
+from uuid import uuid4
+from re import findall
+from curl_cffi.requests import get, RequestsError
+
+class youChat:
+    def create(self, prompt):
+        resp = get(
+            "https://you.com/api/streamingSearch",
+            headers={
+                "cache-control": "no-cache",
+                "referer": "https://you.com/search?q=gpt4&tbm=youchat",
+                "cookie": f"safesearch_guest=Off; uuid_guest={str(uuid4())}",
+            },
+            params={
+                "q": prompt,
+                "page": 1,
+                "count": 10,
+                "safeSearch": "Off",
+                "onShoppingPage": False,
+                "mkt": "",
+                "responseFilter": "WebPages,Translations,TimeZone,Computation,RelatedSearches",
+                "domain": "youchat",
+                "queryTraceId": str(uuid4()),
+                "chat": [],
+            },
+            impersonate="chrome107",
+        )
+        if "youChatToken" not in resp.text:
+            raise RequestsError("Unable to fetch the response.")
+        return (
+            "".join(
+                findall(
+                    r"{\"youChatToken\": \"(.*?)\"}",
+                    resp.content.decode("unicode-escape"),
+                )
+            )
+            .replace("\\n", "\n")
+            .replace("\\\\", "\\")
+            .replace('\\"', '"')
+        )
+
+    @staticmethod
+    def chat_cli(prompt):
+        """Generate completion based on the provided prompt"""
+        you_chat = youChat()
+        completion = you_chat.create(prompt)
+        print(completion)
+        
+@click.option('--prompt', prompt='Enter your prompt', help='The prompt to generate a completion from.')
+def youchat(prompt):
+    youChat.chat_cli(prompt)
 
 API_URL = "https://api-inference.huggingface.co/models/openchat/openchat-3.5-0106"
 IMAGE_API_URL = "https://api-inference.huggingface.co/models/segmind/Segmind-Vega"
