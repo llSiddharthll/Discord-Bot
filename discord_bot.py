554659a27d6895fa3b2f1d9e7d482c5711ab4db7
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-27 19:15:15 +0530

    ok

--- discord_bot.py
+++ discord_bot.py
@@ -2,25 +2,16 @@ import discord
 import requests
 import os
 import io
+from webscout.AI import youChat
+
+# Instantiate the youchat class
+youChat = youChat()
 
 API_URL = "https://api-inference.huggingface.co/models/openchat/openchat-3.5-0106"
 IMAGE_API_URL = "https://api-inference.huggingface.co/models/segmind/Segmind-Vega"
 headers = {"Authorization": "Bearer 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"}
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
-def query(payload):
-    formatted_payload = f"""
-        GPT4 Correct User: Hello<|end_of_turn|>
-        GPT4 Correct Assistant: Hi<|end_of_turn|>
-        GPT4 Correct User: What is your name?<|end_of_turn|>
-        GPT4 Correct Assistant: My name is "Itachi Uchiha" of the village "leaf", I am a conversational bot made by Siddharth<|end_of_turn|>
-        GPT4 Correct User: {payload}<|end_of_turn|>
-        GPT4 Correct Assistant: 
-        """
-    response = requests.post(
-        API_URL, headers=headers, json={"inputs": formatted_payload}
-    )
-    return response.json()
 	
 def query_image(payload):
     response = requests.post(IMAGE_API_URL, headers=headers, json=payload)
@@ -39,13 +30,8 @@ class MyClient(discord.Client):
 
             if user_input.startswith(("itachi", "/bro", "bro", "jade", "bot")):
                 async with message.channel.typing():
-                    output = query(user_input)
-                    generated_text = output[0]["generated_text"]
-                    output_index = generated_text.find(user_input)
-                    output_text = generated_text[output_index + len(user_input):]
-                    lines = output_text.split('\n')
-                    result_output = '\n'.join(lines[2:])
-                    await message.channel.send(result_output)
+                    output = youChat.create(user_input)
+                    await message.channel.send(output)
 
             elif user_input.startswith(("generate", "make")):
                 async with message.channel.typing():
